94917c2545b50a575a239f7a99867c5273284c65
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-13 20:42:48 +0100

    Update CalculatorFXMLController.java
    zabezpieczenia przed złymi znakami

--- CalculatorFXMLController.java
+++ CalculatorFXMLController.java
@@ -4,13 +4,19 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
+/**
+ * Klasa <code>CalculatorFXMLController</code> reprezentuje działanie kalkulatora. 
+ * Posiada pola tekstowe do wprowadzania wartości, pole wynikowe oraz przełączniki 
+ * wyboru działania. Działania wykonywane są automatycznie po zmianie zawartości pól 
+ * lub po zmianie typu operacji. 
+ * @author AleksanderSklorz
+ */
 public class CalculatorFXMLController implements Initializable {
     @FXML
     private RadioButton additionRadioButton, subtractionRadioButton, multiplicationRadioButton, divisionRadioButton; 
@@ -18,10 +24,6 @@ public class CalculatorFXMLController implements Initializable {
     private Label operationLabel;
     @FXML
     private TextField firstNumberTextField, secondNumberTextField, resultTextField;
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
@@ -47,11 +49,16 @@ public class CalculatorFXMLController implements Initializable {
         secondNumberTextField.textProperty().addListener(listener -> {
             calculate();
         });
-    }    
+    }
+    /**
+     * Wykonuje wybrane działanie. Aktualne działanie jest wyświetlone jako etykieta
+     * pomiędzy wprowadzonymi wartościami liczbowymi. Wyświetla komunikat w przypadku wprowadzenia
+     * niedozwolonych wartości (innych niż liczby całkowite lub zmiennoprzecinkowe). 
+     */
     public void calculate(){
         char operation = operationLabel.getText().charAt(0); 
         String sFirstNumber = firstNumberTextField.getText().trim(), sSecondNumber = secondNumberTextField.getText().trim();
-        if(sFirstNumber.matches("[0-9]+") && sSecondNumber.matches("[0-9]+")){
+        if(sFirstNumber.matches("[0-9]([\\.][0-9]+|[0-9]*)") && sSecondNumber.matches("[0-9]([\\.][0-9]+|[0-9]*)")){
             BigDecimal firstNumber = BigDecimal.valueOf(Double.parseDouble(sFirstNumber)),
                     secondNumber = BigDecimal.valueOf(Double.parseDouble(sSecondNumber));
             switch(operation){
